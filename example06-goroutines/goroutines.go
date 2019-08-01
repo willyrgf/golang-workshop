3ee49eaee728c46998e11b0149ba4234e18f47f0
Willy Romão <willyr.goncalves@gmail.com>
2019-08-01 13:51:05 -0300

    improve goroutines examples

--- example06-goroutines/goroutines.go
+++ example06-goroutines/goroutines.go
@@ -7,16 +7,16 @@ import (
 )
 
 func f(threadNumber string) {
-	for i := 0; i < 10; i++ {
-		fmt.Println(threadNumber, ":", i)
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("%s, count=%d\n", threadNumber, i)
 		amt := time.Duration(rand.Intn(250))
 		time.Sleep(time.Millisecond * amt)
 	}
 }
 
 func main() {
-	for i := 0; i < 3; i++ {
-		go f(fmt.Sprintf("thread number: %d", i))
+	for i := 1; i <= 3; i++ {
+		go f(fmt.Sprintf("thread_number=%d", i))
 	}
 	var input string
 	fmt.Scanln(&input)
