0dc15e01a1f7a0c1b3874a03afaab744fc8fc627
Willy Romão <willyr.goncalves@gmail.com>
2019-08-02 12:56:42 -0300

    add sync Wait.Group on go routinines example

--- example06-goroutines/goroutines.go
+++ example06-goroutines/goroutines.go
@@ -3,21 +3,25 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
-func f(threadNumber string) {
+func f(threadNumber string, wg *sync.WaitGroup) {
 	for i := 1; i <= 10; i++ {
 		fmt.Printf("%s, count=%d\n", threadNumber, i)
 		amt := time.Duration(rand.Intn(250))
 		time.Sleep(time.Millisecond * amt)
 	}
+	wg.Done()
 }
 
 func main() {
+	var wg sync.WaitGroup
 	for i := 1; i <= 3; i++ {
-		go f(fmt.Sprintf("thread_number=%d", i))
+		wg.Add(1)
+		go f(fmt.Sprintf("thread_number=%d", i), &wg)
 	}
-	var input string
-	fmt.Scanln(&input)
+	wg.Wait()
+	fmt.Println("finish")
 }
